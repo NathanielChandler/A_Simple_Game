98eb8a046f7eedf4ea3d0dc20757f5dd3bd7f94f
NathanielChandler <chandlerocks4@gmail.com>
2017-03-22 11:10:44 -0700

    3/22 Progress 1

--- A_Simple_Game/Assets/Scripts/PlayerAttack.cs
+++ A_Simple_Game/Assets/Scripts/PlayerAttack.cs
@@ -3,28 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
+    public int swingTime;
+    private bool leftClick;
+    private Animator attackAnim;
 
-    public float speed = 1.0F;
-    private float startTime;
-    private float journeyLength;
-    private Transform swordTransform;
-    public GameObject Sword;
-    
     void Start()
     {
-        startTime = Time.time;
-        swordTransform = Sword.transform;
-        journeyLength = Vector3.Distance(swordTransform.position, new Vector3(1, 0, 1));
-        
+        attackAnim = GetComponent<Animator>();
+       // leftClick = attackAnim.GetBool("LeftClick");
     }
-    
+
+
     void FixedUpdate()
     {
-        float distCovered = (Time.time - startTime) * speed;
-        float fracJourney = distCovered / journeyLength;
-        Vector3 swordGoTo = new Vector3(swordTransform.localPosition.x + 1, swordTransform.localPosition.y, swordTransform.localPosition.z);
-        swordTransform.position = Vector3.Lerp(swordTransform.localPosition, swordGoTo, fracJourney);
+        if(Input.GetMouseButtonDown(0))
+        {
+           // leftClick = true;
+           // attackAnim.SetBool("LeftClick", leftClick);
+            attackAnim.SetTrigger("LeftClicked");
+            
+        }
     }
-    
-    
 }
