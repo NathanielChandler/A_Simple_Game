1aa96179b077eff8cd2574a25a98b383a0e99231
Nathaniel <chandlerocks4@gmail.com>
2017-03-21 23:59:15 -0700

    Moving Progress to GitHub
    Don't lose the keys

---
+++ A_Simple_Game/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    public float speed = 1.0F;
+    private float startTime;
+    private float journeyLength;
+    private Transform swordTransform;
+    public GameObject Sword;
+    
+    void Start()
+    {
+        startTime = Time.time;
+        swordTransform = Sword.transform;
+        journeyLength = Vector3.Distance(swordTransform.position, new Vector3(1, 0, 1));
+        
+    }
+    
+    void FixedUpdate()
+    {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        Vector3 swordGoTo = new Vector3(swordTransform.localPosition.x + 1, swordTransform.localPosition.y, swordTransform.localPosition.z);
+        swordTransform.position = Vector3.Lerp(swordTransform.localPosition, swordGoTo, fracJourney);
+    }
+    
+    
+}
---
+++ A_Simple_Game/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveMod;
+    public float turnSpeed;
+
+
+    private Vector3 movement;
+   // private Rigidbody playerRigidbody;
+
+    // Use this for initialization
+    void Start () {
+       // playerRigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turn();
+	}
+
+    void Move(float h, float v)
+    {
+        movement.Set(h, 0, v);
+        movement = movement.normalized * moveMod * Time.deltaTime;
+        //playerRigidbody.MovePosition(transform.position + movement);
+        transform.Translate(movement);
+    }
+
+    void Turn()
+    {
+        float direction = 0;
+        if(Input.mousePosition.x > 5 * Screen.width/9)
+        {
+            direction = Input.mousePosition.x / Screen.width;
+        }
+        else if (Input.mousePosition.x < 4 * Screen.width / 9)
+        {
+            direction = -1 * (1 - Input.mousePosition.x / Screen.width);
+        }
+        Vector3 manip = new Vector3(0, direction * turnSpeed, 0);
+        transform.Rotate(manip);
+       // playerRigidbody.MoveRotation(Quaternion.Euler(manip));
+    }
+}
