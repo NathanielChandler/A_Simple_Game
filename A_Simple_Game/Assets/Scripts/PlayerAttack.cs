1645b9fd7bd181655f71297ceadfb872385a8dd9
Nathaniel <chandlerocks4@gmail.com>
2017-03-22 23:17:13 -0700

    3/22 Progress 2: Step 3 Done

--- A_Simple_Game/Assets/Scripts/PlayerAttack.cs
+++ A_Simple_Game/Assets/Scripts/PlayerAttack.cs
@@ -3,25 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour {
-    public int swingTime;
+    public float swingTime;
+
     private bool leftClick;
     private Animator attackAnim;
+    private float timer;
+    private PlayerMovement playerMovement;
+    private float defaultMoveMod;
+    private float defaultTurnSpeed;
 
     void Start()
     {
         attackAnim = GetComponent<Animator>();
-       // leftClick = attackAnim.GetBool("LeftClick");
+        playerMovement = GetComponent<PlayerMovement>();
+        // leftClick = attackAnim.GetBool("LeftClick");
+        defaultMoveMod = playerMovement.moveMod;
+        defaultTurnSpeed = playerMovement.turnSpeed;
     }
 
 
     void FixedUpdate()
     {
-        if(Input.GetMouseButtonDown(0))
+        timer += Time.deltaTime;
+        if(Input.GetMouseButtonDown(0) && timer >= swingTime)
         {
+            timer = 0;
            // leftClick = true;
            // attackAnim.SetBool("LeftClick", leftClick);
             attackAnim.SetTrigger("LeftClicked");
+            playerMovement.turnSpeed = 1;
+            playerMovement.moveMod = 1;
             
+        }
+        if(timer >= 1)
+        {
+            playerMovement.turnSpeed = defaultTurnSpeed;
+            playerMovement.moveMod = defaultMoveMod;
         }
     }
 }
