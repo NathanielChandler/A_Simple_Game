7ad3d746384db599a683a7b3144020b1e5375474
ncc <ncc>
2017-03-27 10:16:48 -0700

    3/27 Progress 2

---
+++ A_Simple_Game/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public GameObject destination;
+    private Vector3 destinationLocation;
+    private Mes
+
+	void Start () {
+        destinationLocation = destination.transform.localPosition;
+	}
+	
+	void FixedUpdate () {
+    file:///C:/Program%20Files/Unity/Editor/Data/Documentation/en/ScriptReference/AI.NavMeshAgent-destination.html
+	}
+}
