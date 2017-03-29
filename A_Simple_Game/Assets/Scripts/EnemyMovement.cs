cf0228ab84c8e461b3d10ca772a3cd9e91825e54
Nathaniel <chandlerocks4@gmail.com>
2017-03-29 16:44:08 -0700

    3/29 Progress 1

--- A_Simple_Game/Assets/Scripts/EnemyMovement.cs
+++ A_Simple_Game/Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI; 
 
-public class EnemyMovement : MonoBehaviour {
+public class EnemyMovement : MonoBehaviour
+{
 
-    public GameObject destination;
-    private Vector3 destinationLocation;
-    private Mes
+    public GameObject target;
+    private Vector3 targetLocation;
+    private NavMeshAgent agent;
 
-	void Start () {
-        destinationLocation = destination.transform.localPosition;
+	void Start ()
+    {
+        targetLocation = target.transform.localPosition;
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = targetLocation;
 	}
 	
-	void FixedUpdate () {
-    file:///C:/Program%20Files/Unity/Editor/Data/Documentation/en/ScriptReference/AI.NavMeshAgent-destination.html
-	}
+	void FixedUpdate ()
+    {
+        //file:///C:/Program%20Files/Unity/Editor/Data/Documentation/en/ScriptReference/AI.NavMeshAgent-destination.html
+        if (Vector3.Distance(targetLocation, target.transform.position) > 1.0f)
+        {
+            targetLocation = target.transform.position;
+            agent.destination = targetLocation;
+        }
+
+    }
 }
